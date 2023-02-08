f70c46740916c01db1aaddd6c700d2748503281c
Olga <olyusik.soloveva@mail.ru>
2023-02-09 01:17:18 +0300

    111

--- 341.py
+++ 341.py
@@ -1,17 +1,18 @@
 import json
-register = {}
-with open ('reg.json', 'w') as f:
-    json.dump(register,f)
-log = str (input("введите логин:"))
-password = str (input("введите пароль:"))
 
+register = {}
+with open ('reg.json', 'w') as file:
+    json.dump(register,file)
+
+log = int(input('Введите логин: '))
+password = int (input('Введите пароль:'))
 def regist (log, password):
-    with open('reg.json', 'r') as f:
-        register = json.load(f)
-    if log not in register.keys():
-        register[log] = password
-        with open ("reg.json", "w") as f:
-            с
+    with open('reg.json', 'r') as file:
+        reg = json.load(file)
+    if log not in reg:
+        reg[log] = password
+        with open ("reg.json", "w") as file:
+            json.dump(reg,file)
         print ('регистрация прошла успешно')
     else:
-        print ('аккаунт с таким логином уже существует')
+        print('Такой логин уже существует')
